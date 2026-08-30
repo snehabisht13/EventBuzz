dd2dd2f70a1de8c030381dd7f9091638b8ccfb80
Sneha <sneha.bisht@yabx.co>
2026-08-30 19:37:34 +0530

    configured production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,23 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch("APP_HOST", "eventbuzz-production.up.railway.app"),
+    protocol: "https"
+  }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch("SMTP_ADDRESS", "smtp.gmail.com"),
+    port: ENV.fetch("SMTP_PORT", 587),
+    domain: ENV.fetch("SMTP_DOMAIN", "gmail.com"),
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: ENV.fetch("SMTP_AUTH", "plain"),
+    enable_starttls_auto: true
+  }
 end
